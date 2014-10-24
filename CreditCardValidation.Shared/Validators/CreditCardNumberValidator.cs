54b4d937d02ee5f497ce9239169cc7bea8d9810c
Viktor Hofer <viktor.hofer@outlook.com>
2014-10-24 17:22:56 +0200

    Initial commit

---
+++ CreditCardValidation.Shared/Validators/CreditCardNumberValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace CreditCardValidation.Validators
+{
+    public class CreditCardNumberValidator : AbstractValidator<string>
+    {
+        public CreditCardNumberValidator()
+        {
+            RuleFor(creditCardNumber => creditCardNumber).NotNull()
+                .WithName("CreditCardNumber");
+
+            RuleFor(creditCardNumber => creditCardNumber).Length(16)
+                .WithMessage("Credit card number must have 16 letters");
+        }
+    }
+}
---
+++ CreditCardValidation.Shared/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CreditCardValidation.Validators;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardValidation.Common.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly CreditCardNumberValidator _creditCardNumberValidator;
+        private readonly INavigationService _navigationService;
+
+        public RelayCommand<string> AcceptCreditCardNumberCommand { get; private set; }
+
+        public MainViewModel(INavigationService navigationService,
+            CreditCardNumberValidator creditCardNumberValidator)
+        {
+            _navigationService = navigationService;
+            _creditCardNumberValidator = creditCardNumberValidator;
+
+            AcceptCreditCardNumberCommand = new RelayCommand<string>(onAcceptCreditCardNumberExecuted, onAcceptCreditCardNumberCanExecute);
+        }
+
+        private void onAcceptCreditCardNumberExecuted(string creditCardNumber)
+        {
+            _navigationService.NavigateTo(ViewModelLocator)
+        }
+
+        private bool onAcceptCreditCardNumberCanExecute(string creditCardNumber)
+        {
+            return _creditCardNumberValidator.Validate(creditCardNumber).IsValid;
+        }
+    }
+}
